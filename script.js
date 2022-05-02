e7f804c2e8079d9196217518cc893029be9d5538
fabifelicia <fabianabarretomenezes@gmail.com>
2022-05-02 17:33:23 -0300

    add files portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function displayAbout() {
+    let arrow = document.getElementById('animation')
+    arrow.classList.toggle('rotate')    
+
+    let about = document.getElementById('about')
+    about.classList.toggle('active')    
+}
